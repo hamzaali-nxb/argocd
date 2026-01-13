eb6886c799cfa90ea2146416fdd0e54f2cde42ae
hamza ali <hamza.ali@cloudforce-1.com>
2026-01-13 16:11:40 +0500

    no pangaaa

--- app/index.js
+++ app/index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200);
-  res.end(" han g Hamza, Welcome to ArgoCD + Helm + GitHub Actions World!\n");
+  res.end(" HAHAHAHA! Hamza, Welcome to ArgoCD + Helm + GitHub Actions World!\n");
 });
 
 server.listen(port, () => {
